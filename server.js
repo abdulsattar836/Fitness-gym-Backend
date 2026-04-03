0a5d9aa6c633166c2691dd7707d7cc1d3ef0454a
abdulsattar836 <abdulsattar836@github.com>
2026-04-03 18:19:45 +0500

    first commit

--- server.js
+++ server.js
@@ -82,43 +82,63 @@ app.use(globalErrorHandler);
 // ==================================================
 // 🔹 MONGODB CONNECTION WITH CACHED PROMISE
 // ==================================================
-let cached = global.mongoose;
+// let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
 
-async function connectDB() {
-  if (cached.conn) return cached.conn;
+// async function connectDB() {
+//   if (cached.conn) return cached.conn;
 
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(process.env.mongo_uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then((mongoose) => {
-        console.log("✅ MongoDB connected");
-        return mongoose;
-      })
-      .catch((err) => {
-        console.error("❌ MongoDB connection error:", err);
-        process.exit(1); // exit if DB connection fails
-      });
+//   if (!cached.promise) {
+//     cached.promise = mongoose
+//       .connect(process.env.mongo_uri, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       })
+//       .then((mongoose) => {
+//         console.log("✅ MongoDB connected");
+//         return mongoose;
+//       })
+//       .catch((err) => {
+//         console.error("❌ MongoDB connection error:", err);
+//         process.exit(1); // exit if DB connection fails
+//       });
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+let isConnected = false;
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.mongo_uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("Connected too MongoDB");
+  } catch (error) {
+    console.log("Error connecting to DB", error);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
 
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectToMongoDB();
+  }
+  next();
+});
 // ==================================================
 // 🔹 START SERVER AFTER DB CONNECTED
 // ==================================================
-connectDB().then(() => {
-  const PORT = process.env.PORT || 5000;
-  server.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
-});
+// connectDB().then(() => {
+//   const PORT = process.env.PORT || 5000;
+//   server.listen(PORT, () => {
+//     console.log(`🚀 Server running on port ${PORT}`);
+//   });
+// });
 
 module.exports = app;
