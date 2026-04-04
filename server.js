a32edfc434b876ec931d00b9c1dbf51b031731c1
abdulsattar836 <abdulsattar836@github.com>
2026-04-04 10:25:11 +0500

    first commit

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.use((req, res, next) => {
 // 🔹 START SERVER AFTER DB CONNECTED
 // ==================================================
 // connectDB().then(() => {
-//   const PORT = process.env.PORT || 5000;
+//   const PORT = process.env.PORT || 4000;
 //   server.listen(PORT, () => {
 //     console.log(`🚀 Server running on port ${PORT}`);
 //   });
